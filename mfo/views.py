b6516cde66d0f7610cba74d8406c3bf089d84ad5
ryanrushton <ryan.rushton79@gmail.com>
2016-12-09 20:36:52 +1100

    Extended file system viewer to so that you can go backwards in dirs

--- mfo/views.py
+++ mfo/views.py
@@ -11,14 +11,16 @@ def index(request):
     print(current_dir)
     child_dirs = []
     child_files = []
-    if current_dir is None:
+    if current_dir is None or current_dir == base_dir:
         current_dir = base_dir
-        tmp = os.listdir(current_dir)
-        for item in tmp:
-            if item[0] != '.' and os.path.isdir(os.path.join(current_dir, item)):
-                child_dirs.append((item, os.path.join(current_dir, item)))
-            elif item[0] != '.':
-                child_files.append((item, os.path.join(current_dir, item)))
+    else:
+        child_dirs.append(('..', os.path.split(current_dir)[0]))
+    tmp = os.listdir(current_dir)
+    for item in tmp:
+        if item[0] != '.' and os.path.isdir(os.path.join(current_dir, item)):
+            child_dirs.append((item, os.path.join(current_dir, item)))
+        elif item[0] != '.':
+            child_files.append((item, os.path.join(current_dir, item)))
     return render(request, 'mfo/index.html', {
         'current_dir': current_dir,
         'child_dirs': child_dirs,
